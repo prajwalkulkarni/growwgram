4debe06e68a62ab19f22caea8e0a4097857977f4
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-08-04 15:04:22 +0530

    Update PostItem

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,35 +1,39 @@
 import React from "react";
 import './PostItem.css'
-import Card from '../Card/Card';
 import { PostItemType } from "../../types/types";
-const PostItem: React.FC<PostItemType> = (props) => {
+import { Link } from "react-router-dom";
+const PostItem = React.forwardRef<HTMLDivElement,PostItemType>((props,ref) => {
     return (
-        
-            <div className="post-item">
-                <div className="post-item__image">
-                    <img src= {props.urls.small} />
+
+        <div className="post-item" ref={ref}>
+            <div className="post-item__image">
+                <img src={props.urls.small} />
+            </div>
+
+            <div className="post-item__content">
+
+                <div className="post-item__imageDetails">
+                    <div className="post-item__imageDetails__numbers">
+                        <p>Created At {props.created_at}</p>&nbsp;&nbsp;&nbsp;
+                        <p>Likes {props.likes}</p>
+                    </div>
+                    <article>Description</article>
                 </div>
-
-                <div className="post-item__content">
-
-                    <div className="post-item__imageDetails">
-                        <div className="post-item__imageDetails__numbers">
-                            <p>Created At {props.created_at}</p>
-                            <p>Likes {props.likes}</p>
-                        </div>
-                        <article>Description</article>
-                    </div>
-                    <div className="post-item__profileInfo">
-                        <img src={require('../../assets/logo.png')} height="50" width={50} />
-                        <p>Name: {props.user.name}</p>
-                    </div>
-
+                <div className="post-item__profileInfo">
+                    <Link to={`/user/${props.user.username}`}>
+                        <img src={props.user.profile_image?.medium} height="50" width={50} />
+                    </Link>
+                    <Link to={`/user/${props.user.username}`}>
+                        <p><b>{props.user.name}</b></p>
+                    </Link>
                 </div>
 
             </div>
-        
+
+        </div>
+
 
     )
-}
+})
 
 export default PostItem
