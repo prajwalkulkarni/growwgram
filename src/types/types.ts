721110f9abc398c4da530ecea470f931fa50b5f4
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-08-06 14:38:37 +0530

    Add type definition for post & profile type

---
+++ src/types/types.ts
@@ -0,0 +1,55 @@
+export type PostItemType = {
+    id: number;
+    ref?: (node: HTMLDivElement)=>void;
+    created_at: string;
+    description: string|null;
+    likes: number;
+    urls: {
+        full?: string;
+        regular?: string;
+        small: string;
+        thumb?: string;
+    };
+    user: {
+        id?: string;
+        name: string;
+        username: string;
+        location?: string|null;
+        profile_image?:{
+            small?: string;
+            medium?: string;
+            large?: string;
+        };
+    };
+}
+
+
+export type ProfileType = {
+    name: string | undefined;
+    username: string | undefined;
+    bio: string|null| undefined;
+    location: string|null| undefined;
+    profile_image: {
+        small?: string;
+        medium?: string;
+        large?: string;
+    };
+    likes: number;
+    photos?:[{
+        id: string,
+        created_at: string,
+        blur_hash: string,
+        urls: {
+            regular: string,
+            small: string,
+            thumb: string,
+                
+        }
+        },    
+    ];
+    social: {
+        instagram_username: string|null,
+        
+        twitter_username: string|null,
+    },
+}
