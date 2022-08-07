e1e310579e77b6f0769c5cf5f7a4cf891f4d89e4
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-08-07 16:26:03 +0530

    Remove active and focus color

--- src/components/Profile/SwitchView.tsx
+++ src/components/Profile/SwitchView.tsx
@@ -3,16 +3,18 @@ import React from "react"
 import './SwitchView.css'
 import { BsListUl, BsGrid3X3GapFill } from "react-icons/bs"
 import Card from "../UI/Card/Card"
+import { useStore } from "../../store/store"
 const SwitchView: React.FC<{ setViewType: Function }> = (props) => {
-
+    
+    const theme = useStore(state=>state.theme)
     return (
-        <Card className="cardstyle">
-            <div className="switchview-container">
-                <div className="gridview" onClick={() => props.setViewType("grid")}>
+        <Card className={`cardstyle ${theme}`}>
+            <div className={`switchview-container ${theme}`}>
+                <div className={`gridview ${theme}`} onClick={() => props.setViewType("grid")}>
                     <BsGrid3X3GapFill />
                 </div>
 
-                <div className="listview" onClick={() => props.setViewType("list")}>
+                <div className={`listview ${theme}`} onClick={() => props.setViewType("list")}>
                     <BsListUl />
                 </div>
 
