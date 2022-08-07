3d8765a2de9ff7d1684bf22cab7e2f11fb46e21b
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-08-07 16:30:27 +0530

    Add misc states and move type defn to types.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,25 @@
 import create from "zustand";
-import { PostItemType } from "../types/types";
+import { PostItemType, PostState } from "../types/types";
 
 
-type PostState = {
-    posts: PostItemType[];
-    username: string;
-    pageCount: number;
-    resetPageCount: () => void;
-    incrementPageCount: () => void;
-    addPosts: (posts: PostItemType[]) => void;
-    setUsername: (username: string) => void;
-    resetUsername: () => void;
-    resetPosts: () => void;
-}
+
 export const useStore = create<PostState>((set, get) => ({
+    theme:"dark",
     pageCount: 1,
     username:'elijahp',
+    isLoading: false,
+    newsFeedPosts: [],
     posts: [],
+    hasMore: true,
     resetPageCount: () => set({ pageCount: 1 }),
     incrementPageCount: () => set((state)=>({pageCount:state.pageCount+1})),
     addPosts: (postsCollection:PostItemType[]) => set((state)=>({posts : [...state.posts,...postsCollection]})),
+    setNewsFeedPosts: (newPost:PostItemType) => set((state)=>({newsFeedPosts : [...state.newsFeedPosts,newPost]})),
+    setNewsFeedPostsOnReload: (loadedposts:PostItemType[]) => set((state)=>({newsFeedPosts : [...loadedposts]})),
     resetPosts: () => set({posts:[]}),
     setUsername: (username:string) => set({username}),
+    setIsLoading: (isLoading:boolean) => set({isLoading}),
+    setHasMore: (hasMore:boolean) => set({hasMore}),
+    setTheme: (theme:string) => set({theme}),
     resetUsername: () => set({username:''})
 }));
