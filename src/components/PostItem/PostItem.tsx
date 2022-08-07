f10b1f65b24e34278250de25e9ca2dcedefc7eb8
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-08-07 16:24:12 +0530

    Add misc features

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -7,20 +7,20 @@ import { useStore } from "../../store/store";
 
 const PostItem = React.forwardRef<HTMLDivElement,PostItemType>((props,ref) => {
 
-    
-
+    const setUsername = useStore(state=>state.setUsername)
+    const theme = useStore(state=>state.theme)
     const date = new Date(props.created_at).toString().split(" ").slice(0,4).join(" ")
     return (
 
-        <div className="post-item" ref={ref}>
+        <div className={`post-item ${theme}`} ref={ref}>
             <div className="post-item__image">
-                <img src={props.urls.small} />
+                <img src={props.urls.small} loading="lazy" />
             </div>
 
-            <div className="post-item__content">
+            <div className={`post-item__content ${theme}`}>
 
                 <div className="post-item__imageDetails">
-                    <div className="post-item__imageDetails__numbers">
+                    <div className={`post-item__imageDetails__numbers ${theme}`}>
                         <h4><AiFillClockCircle/> {`${date}`}</h4>&nbsp;&nbsp;&nbsp;
                         <h4><AiFillHeart/> {props.likes}</h4>
                     </div>
@@ -29,10 +29,10 @@ const PostItem = React.forwardRef<HTMLDivElement,PostItemType>((props,ref) => {
                 </div>
                 <hr/>
                 <div className="post-item__profileInfo">
-                    <Link to={`/user/${props.user.username}`}>
-                        <img src={props.user.profile_image?.medium} height="50" width={50} />
+                    <Link to={`/user/${props.user.username}`} >
+                        <img src={props.user.profile_image?.medium} height="50" width={50} loading="lazy" />
                     </Link>
-                    <Link to={`/user/${props.user.username}`}>
+                    <Link to={`/user/${props.user.username}`} >
                         <h4>{props.user.name}</h4>
                     </Link>
                 </div>
@@ -46,3 +46,5 @@ const PostItem = React.forwardRef<HTMLDivElement,PostItemType>((props,ref) => {
 })
 
 export default PostItem
+
+//onClick={()=>setUsername(props.user.username)}
